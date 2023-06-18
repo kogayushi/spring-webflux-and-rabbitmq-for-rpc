c2664d03ff7a0926d284728a3751b71ef3643cd7
koga yushi <youshe.old.pleasure@gmail.com>
2023-06-18 13:40:51 +0900

    initial commit

---
+++ src/main/kotlin/kogayushi/spring_webflux/and_rabbitmq_forrpc/SpringWebfluxAndRabbitmqForRpcApplication.kt
@@ -0,0 +1,11 @@
+package kogayushi.spring_webflux.and_rabbitmq_forrpc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebfluxAndRabbitmqForRpcApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringWebfluxAndRabbitmqForRpcApplication>(*args)
+}
---
+++ src/test/kotlin/kogayushi/spring_webflux/and_rabbitmq_forrpc/SpringWebfluxAndRabbitmqForRpcApplicationTests.kt
@@ -0,0 +1,13 @@
+package kogayushi.spring_webflux.and_rabbitmq_forrpc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringWebfluxAndRabbitmqForRpcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
